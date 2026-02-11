3f3ddb88f78bdadf3f5b0ea5c06528f018119114
VIKRAM T <159120731+vikram-code23@users.noreply.github.com>
2026-02-11 14:12:18 +0530

    Update voting_program.py

--- voting_program.py
+++ voting_program.py
@@ -5,8 +5,13 @@ if age >=18:
     if gender in ["M","F","TRANS"]:
         if age >=60:
             print("Go to senior citizen line")
-        else:
-            print("Go to queue 1")
+        elif age >=18 and age <60:
+            if gender == "M":
+                print("Go to male line")
+            elif gender == "F":
+                print("Go to female line")
+            else:
+                print("Go to transgender line")
         vote = input("Enter the politician(TVK/DMK):").upper()
         if vote == "TVK" or vote == "DMK":
             print("you voted successfully for ",vote)
@@ -18,12 +23,13 @@ else:
     print("You are not eligible for this vote")
 
 output:
-Enter a name: vikram
-Enter age : 30
-Enter Gender(M/F/TRANS): m
-Go to queue 1
-Enter the politician(TVK/DMK):tvk
-you voted successfully for  TVK
+Enter a name: sandhiya
+Enter age : 20
+Enter Gender(M/F/TRANS): f
+Go to female line
+Enter the politician(TVK/DMK):dmk
+you voted successfully for  DMK
 
 
 ** Process exited - Return Code: 0 **
+
